77812e9032f5266e7ce686c5850765625401fc71
tysheng <353491983@qq.com>
2017-08-17 09:26:46 +0800

    finished

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -11,11 +11,9 @@
 """
 
 import pickle
-import sys
-sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -26,7 +24,26 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### it's all yours from here forward!
 
-### it's all yours from here forward!  
+def diff_data(origin=False):
+    if origin:
+        return features, features, labels, labels
+    else:
+        return train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
+from sklearn.model_selection import train_test_split
+
+feature_train, feature_test, labels_train, labels_test = diff_data()
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(feature_train, labels_train)
+pred = clf.predict(feature_test)
+from sklearn.metrics import accuracy_score
+
+score_ = accuracy_score(labels_test, pred)
+
+print(score_)
